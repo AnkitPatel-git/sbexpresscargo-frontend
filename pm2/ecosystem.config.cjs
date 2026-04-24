1b0986a6d06036f198fc64146dc78f6fb7980065
AnkitPatel-git <84245868+AnkitPatel-git@users.noreply.github.com>
2026-04-24 11:54:23 +0530

    refactor of pm2

--- pm2/ecosystem.config.cjs
+++ pm2/ecosystem.config.cjs
@@ -15,7 +15,8 @@ module.exports = {
       instances: 1,
       exec_mode: "fork",
       autorestart: true,
-      max_memory_restart: "500M",
+      node_args: '--max-old-space-size=1024',
+      max_memory_restart: '800M',
       env: {
         NODE_ENV: "production",
         PORT: "3000",
